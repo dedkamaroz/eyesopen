bec4b169031cd24c4583bfabfa0debc8818a3ad2
sublations <143057905+sublations@users.noreply.github.com>
2023-10-13 23:12:01 -0700

    package src

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
-from pathlib import Path
 
 setup(
     name='EyesOpen',
     version='0.1.0',
-    packages=find_packages(),
-    install_requires = Path('requirements.txt').read_text().splitlines(),
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=open('requirements.txt').read().splitlines(),
     include_package_data=True
 )
