568365ba1cf9bbb7a75d9359c99e3205b8459532
sublations <143057905+sublations@users.noreply.github.com>
2023-10-13 22:57:54 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup, find_packages
+from pathlib import Path
 
 setup(
     name='EyesOpen',
     version='0.1.0',
     packages=find_packages(),
-    install_requires=open('requirements.txt').read().splitlines(),
+    install_requires = Path('requirements.txt').read_text().splitlines(),
     include_package_data=True
 )
